8a1460a0bc1ecf0de029730b994ba57a59dc3c8d
Nathan Long <him@nathanmlong.com>
2016-01-15 10:30:33 -0500

    more stuff

--- parallel_pids2.exs
+++ parallel_pids2.exs
@@ -1,25 +1,32 @@
 defmodule Mappy do
-  def m(coll, :stream) do
-    IO.puts "stream got a collection"
+  def stream(coll, ident, func) do
+    IO.puts "stream #{ident} got a collection"
     :timer.sleep(1_000)
     Stream.map(coll, fn (num) ->
-      IO.puts "stream got item #{num}"
-      num
+      :timer.sleep(500)
+      IO.puts "stream #{ident} got item #{num}"
+      func.(num)
     end)
   end
 
-  def m(coll, :sync) do
-    IO.puts "sync   got a collection"
+  def sync(coll, ident, func) do
+    IO.puts "sync   #{ident}  got a collection"
     :timer.sleep(1_000)
     Enum.map(coll, fn (num) ->
-      IO.puts "sync   got item #{num}"
-      num
+      :timer.sleep(500)
+      IO.puts "sync   #{ident} got item #{num}"
+      func.(num)
     end)
   end
 end
 
-1..3 
-|> Mappy.m(:stream)
-|> Mappy.m(:stream)
-|> Mappy.m(:sync)
+1..3
+|> Mappy.stream(:one, &(&1))
+|> Mappy.stream(:two, &(&1))
+|> Mappy.sync(:one, &(&1))
 |> IO.inspect
+
+# This is not good enough because only one item is traveling down the conveyor belt at a time. I went to hear a chorus of "starting work" from all machines as they each get a new item simultaneously.
+
+# Stream.resource or transform
+# webfaction - michael/elixir/stream.ex
