302e90088016ad0e67c2f23395fade221f33aad6
Michael Gundlach <gundlach@gmail.com>
2016-01-25 22:11:40 +0000

    Better demo

--- learning/demo4.exs
+++ learning/demo4.exs
@@ -1,3 +1,11 @@
 require Steve
 
-Steve.wrap([1,2,3]) |> Enum.to_list |> IO.inspect
+x = [1,2,3]
+y = Stream.map(x, &(&1 + 2))
+z = Enum.to_list(y)
+IO.inspect(z)
+
+x = Steve.wrap([1,2,3])
+y = Steve.wrap(Stream.map(x, &(&1 + 2)))
+z = Enum.to_list(y)
+IO.inspect(z)
