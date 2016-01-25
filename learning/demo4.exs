8fac45d3fb91d5b3c2d6d71645c54d0ee7336ff5
Michael Gundlach <gundlach@gmail.com>
2016-01-25 22:15:27 +0000

    Even better

--- learning/demo4.exs
+++ learning/demo4.exs
@@ -1,11 +1,13 @@
 require Steve
 
-x = [1,2,3]
-y = Stream.map(x, &(&1 + 2))
-z = Enum.to_list(y)
-IO.inspect(z)
+x = 1..10
+y = Stream.map(x, &(&1 * 3))
+z = Stream.filter(y, &(rem(&1, 2) == 0))
+w = Enum.to_list(z)
+IO.inspect(w)
 
-x = Steve.wrap([1,2,3])
-y = Steve.wrap(Stream.map(x, &(&1 + 2)))
-z = Enum.to_list(y)
-IO.inspect(z)
+x = Steve.wrap(1..10)
+y = Steve.wrap(Stream.map(x, &(&1 * 3)))
+z = Steve.wrap(Stream.filter(y, &(rem(&1, 2) == 0)))
+w = Enum.to_list(z)
+IO.inspect(w)
