86e8df90e8a0337f8c0a51796c21c082ebdf1117
Michael Gundlach <gundlach@gmail.com>
2016-01-29 14:23:11 +0000

    Comments

--- learning/steve/steve.exs
+++ learning/steve/steve.exs
@@ -8,26 +8,32 @@ defmodule Steve do # "What kind of a wrapping name is 'Steve'?" - FotC
 end
 
 defimpl Enumerable, for: Steve do
-  def reduce(steve, {cmd, acc}, fun) do
+  def reduce(steve, {cmd, acc}, callback) do
     parent = self
     child = spawn_link fn -> 
+      # TODO: limit to 10 in the send queue
       Enum.each(steve.enum, fn el -> send parent, {self, :have_an_element, el} end)
-      send parent, {self, :done}
+      send parent, {self, :thats_all}
     end
 
-    recv(child, {cmd, acc}, fun)
+    recv(child, {cmd, acc}, callback)
   end
 
-  defp recv(child, {:suspend, acc}, fun), do: {:suspended, acc, &(recv(child, &1, fun))}
-  defp recv(child, {:halt, acc}, _fun),   do: halt(child, acc)
-  defp recv(child, {:cont, acc}, fun)     do
+  defp recv(child, {:suspend, acc}, callback), do: {:suspended, acc, &(recv(child, &1, callback))}
+  defp recv(child, {:halt, acc}, _callback),   do: halt(child, acc)
+  defp recv(child, {:cont, acc}, callback)     do
     receive do
-      {^child, :have_an_element, el} -> recv(child, fun.(el, acc), fun)
-      {^child, :done} -> {:done, acc}
+      {^child, :have_an_element, el} -> 
+        {new_cmd, new_acc} = callback.(el, acc)
+        recv(child, {new_cmd, new_acc}, callback)
+      {^child, :thats_all} -> 
+        {:done, acc}
     end
   end
 
   defp halt(child, acc) do
+    # TODO question: if the parent dies, does the child die?
+    # if not, we need to kill the children's children too in this case.
     Process.exit(child, :normal)
     {:halted, acc}
   end
