5b79f15fd121cf29b725d5e5a27354478ab41f6c
Nathan Long <him@nathanmlong.com>
2016-02-05 15:10:12 -0500

    More enum stuff

--- myenum/my_enum.exs
+++ myenum/my_enum.exs
@@ -1,100 +1,133 @@
+# MyEnum reimplements some functions from Enum as a learning exercise, to
+# better understand Enumerable.reduce (which all the Enum functions depend on)
+# To aid in understanding, we think in terms of a story.  An owner has some
+# fruit orchards (enumerable collections). He has some hard-working but stupid
+# servants named Enumerable.reduce (I know, it's a Finnish name).
+# He has some smart but lazy servants, too (anonymous functions passed to Enumerable.reduce).
+# He sends them out in pairs into his orchards to do various work. Eg:
+# - "Go pick all the apples" (to_list)
+# - "Go pick 20 apples" (take)
+# - "Go get all the apples that are ripe" (filter)
+# - "Help me build fruit baskets of one apple and one plum each - fetch one apple and one plum, and repeat until we run out of one of them" (zip)
+# The stupid servant always just picks one fruit and looks to the
+# smart-but-lazy servant to tell him what to do next - pick another, stop, or
+# take a break and wait for the master to summon you again. The smart-but-lazy
+# servant decides what goes into the fruit bag.
+# If the stupid servant finds no fruit to pick, he wanders back to the master
+# in confusion and hands him the fruit bag.
+
 defmodule MyEnum do
   def to_list orchard do
     smart_servant = fn apple, bag ->
+      # put the apple in the bag
       new_bag = [apple | bag]
+      # order the dumb guy to pick another
       {:cont, new_bag}
     end
     first_bag = []
+    # We expect the dumb servant to wander back to us when he can find no more fruit
+    # :done is what he says at that point - "sorry boss, can't find no more"
     {:done, final_bag} = Enumerable.reduce(orchard, {:cont, first_bag}, smart_servant)
     final_bag |> :lists.reverse
   end
 
-  def take(orchard, n) when n >= 0 do
-    smart_servant = fn apple, _cart = {bag, i} ->
-      if i == 0 do
+  # The smart servant works with a cart, containing a bag to put fruit in and
+  # a string of knots, which show how many fruits he should collect. He
+  # unties one knot each time he collects a fruit and stops when there are no
+  # more to untie.
+  def take(orchard, knots) when knots >= 0 do
+    smart_servant = fn apple, _cart = {bag, knots} ->
+      if knots == 0 do
         {:halt, bag}
       else
         new_bag = [apple | bag]
-        new_cart = {new_bag, i - 1}
+        new_cart = {new_bag, knots - 1} # untie one knot
         {:cont, new_cart}
       end
     end
     initial_bag = []
-    initial_cart = {initial_bag, n}
-    {:halted, final_bag} = Enumerable.reduce(orchard, {:cont, initial_cart}, smart_servant)
+    initial_cart = {initial_bag, knots}
+    # We expect the dumb servant to come back and say one of two things:
+    # - :halted, meaning "uh, he told me to stop"
+    # - :done, meaning "I couldn't find no more fruit"
+    # Either way, we don't care - we've taken either what we wanted or as close
+    # as possible
+    {_halted_or_done, final_bag} = Enumerable.reduce(orchard, {:cont, initial_cart}, smart_servant)
     final_bag |> :lists.reverse
   end
 
-  def filter orchard, test do
+  # The boss tells the smart servant some criteria for apples - "only the red
+  # ones" or "only the ripe ones". The smart servant checks each one and always
+  # tells the dumb servant to check for more.
+  def filter orchard, criteria do
     smart_servant = fn apple, bag ->
-      if test.(apple) do
-        new_bag = [apple | bag]
-        {:cont, new_bag}
-      else
-        {:cont, bag}
-      end
+      apple_wanted? = criteria.(apple)
+      new_bag = if apple_wanted?, do: [apple | bag], else: bag
+      {:cont, new_bag}
     end
     initial_bag = []
+    # We expect the dumb servant to wander back to us when he can find no more fruit
+    # :done is what he says at that point - "sorry boss, can't find no more"
     {:done, final_bag} = Enumerable.reduce(orchard, {:cont, initial_bag}, smart_servant)
     final_bag |> :lists.reverse
   end
 
-  # Start with two empty saucers
-  # order a pair of servants to go into each orchard and do the following:
-  # - dummy should pick a fruit and show to lazy
-  # - lazy should tell dummy to bring to me, take a break, and hand me his clipboard
-  # if both dummies bring me a fruit
-  # - make a tiny fruit basket and add to bag
-  # - give clipboard to each dummy, give an empty saucer, and send back to orchard
-  # if one or both dummies bring no fruit
-  # - don't send back to orchard
-  # - go away with my bag
-
-  def zip(apple_orchard, kiwi_orchard) do
+  # This one is more complicated. Two orchards, two pairs of servants. 
+  # The owner wants to build fruit baskets consisting of one apple and one plum.
+  # The dumb servants wear whistles around their necks with which they can be
+  # summoned back to work. The owner sends them into the orchards, and as long
+  # as both dumb servants can bring back one fruit, he keeps summoning them to
+  # pick another. 
+  # As soon as one of them runs out of fruit, he tells the other one to also
+  # stop working.
+  def zip(apple_orchard, plum_orchard) do
     bag_of_baskets = []
 
+    # Favoring "very clear" over DRY...
     apple_lazy_servant = fn apple, saucer ->
       full_saucer = [apple | saucer]
       {:suspend, full_saucer}
     end
-
-    kiwi_lazy_servant = fn kiwi, saucer ->
-      full_saucer = [kiwi | saucer]
+    plum_lazy_servant = fn plum, saucer ->
+      full_saucer = [plum | saucer]
       {:suspend, full_saucer}
     end
 
-    apple_clipboard = fn ({command, saucer}) -> 
-      Enumerable.reduce(apple_orchard, {command, saucer}, apple_lazy_servant)
-    end
+    # "Give me your whistle, you dolt!"
+    {:suspended, nil, apple_whistle} = Enumerable.reduce(apple_orchard, {:suspend, nil}, apple_lazy_servant)
+    {:suspended, nil, plum_whistle} = Enumerable.reduce(plum_orchard, {:suspend, nil}, plum_lazy_servant)
 
-    kiwi_clipboard = fn ({command, saucer}) -> 
-      Enumerable.reduce(kiwi_orchard, {command, saucer}, kiwi_lazy_servant)
-    end
-
-    do_zip(bag_of_baskets, [apple_clipboard, kiwi_clipboard])
+    do_zip(bag_of_baskets, [apple_whistle, plum_whistle])
   end
 
-  def do_zip(current_bag_of_baskets, [apple_clipboard, kiwi_clipboard]) do
+  def do_zip(baskets, [apple_whistle, plum_whistle]) do
     apple_saucer = []
-    kiwi_saucer  = []
+    plum_saucer  = []
 
-    apple_result = apple_clipboard.({:cont, apple_saucer})
-    kiwi_result  = kiwi_clipboard.({:cont, kiwi_saucer})
+    apple_result = apple_whistle.({:cont, apple_saucer})
+    plum_result  = plum_whistle.({:cont, plum_saucer})
+    results = [apple_result, plum_result]
 
-    apple_status = apple_result |> elem(0)
-    kiwi_status  = kiwi_result |> elem(0)
-
-    if Enum.any?([apple_status, kiwi_status], &(&1 == :done)) do
-      current_bag_of_baskets |> :lists.reverse
+    if Enum.any?(results, &(elem(&1, 0) == :done)) do
+      # if any dumb worker is still waiting to be told what to do (eg, he can
+      # find more plums but the other guy cannot find more apples), tell him to
+      # :halt - go home for the day
+      results
+      |> Enum.filter(&(elem(&1, 0) == :suspended))
+      |> Enum.each(fn (result) ->
+        whistle = elem(result, 2)
+        whistle.({:halt, nil})
+      end)
+      baskets |> :lists.reverse
     else
       full_apple_saucer = elem(apple_result, 1)
-      full_kiwi_saucer  = elem(kiwi_result,  1)
-      new_basket = {hd(full_apple_saucer), hd(full_kiwi_saucer)}
-      new_bag_of_baskets = [new_basket | current_bag_of_baskets]
+      full_plum_saucer  = elem(plum_result,  1)
+      new_basket = {hd(full_apple_saucer), hd(full_plum_saucer)}
+      updated_baskets = [new_basket | baskets]
 
-      clipboards = Enum.map([apple_result, kiwi_result], &(elem(&1, 2)))
+      whistles = Enum.map([apple_result, plum_result], &(elem(&1, 2)))
 
-      do_zip(new_bag_of_baskets, clipboards)
+      do_zip(updated_baskets, whistles)
     end
   end
 end
